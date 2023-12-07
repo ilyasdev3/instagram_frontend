f140ea0f32ca80478548a40279ea40688449db24
ilyasdev3 <ilyassanjrani708@gmail.com>
2023-12-07 20:50:07 +0500

    Worked on single post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,70 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import CommentIcon from "@mui/icons-material/Comment";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import { PostType } from "../types/post.types";
 import { useLocation } from "react-router-dom";
+import SinglePostModel from "./SinglePostModel";
+import { AppDispatch } from "../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { selectPostData } from "../store/post/post.selectors";
+import { getPostById } from "../store/post/post.slice";
 
-const Post = ({ desc, img, userId }: PostType) => {
-  // const Post = () => {
+const Post = (post: PostType) => {
+  const [open, setOpen] = React.useState(false);
   const { pathname } = useLocation();
 
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(getPostById(post._id));
+  }, []);
+
+  const handleShowModel = () => {
+    setOpen(true);
+  };
+
   return (
-    <article
-      className={`mt-5 border m-5 p-5 ${
-        pathname === "/explore" &&
-        "hover:scale-105 transform transition duration-300 ease-out cursor-pointer"
-      }`}
-    >
-      {pathname !== "/explore" && (
-        <header className="flex justify-between mb-3">
-          <section className="flex items-center gap-4">
-            <img
-              style={{ borderRadius: "100px" }}
-              className="w-[50px] "
-              // src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
-              src={userId?.profile}
-              alt=""
-            />
-            <h3 className="">{userId?.username}</h3>
-          </section>
-          <section>
-            <MoreVertIcon />
-          </section>
-        </header>
-      )}
-      <main>
+    <>
+      <article
+        className={`mt-5 border m-5 p-5 ${
+          pathname === "/explore" &&
+          "hover:scale-105 transform transition duration-300 ease-out cursor-pointer"
+        }`}
+      >
         {pathname !== "/explore" && (
-          <p className="mb-5">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-          </p>
+          <header className="flex justify-between mb-3">
+            <section className="flex items-center gap-4">
+              <img
+                style={{ borderRadius: "100px" }}
+                className="w-[50px] "
+                src={post?.userId?.profile}
+                alt=""
+              />
+              <h3 className="mt-0">{post?.userId?.username}</h3>
+            </section>
+            <section>
+              <MoreVertIcon />
+            </section>
+          </header>
         )}
-        <img
-          className="rounded-md h-[280px] w-full object-cover"
-          // src="https://images.unsplash.com/photo-1701304027488-726cc32c16e5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8"
-          src={img}
-          alt=""
-        />
-      </main>
-      {pathname !== "/explore" && (
-        <footer className="flex justify-between mt-5">
-          <section className="flex gap-8">
-            <div>
-              <FavoriteBorderIcon />
-              <span className="ml-2">32</span>
-            </div>
-            <div>
-              <CommentIcon />
-              <span className="ml-2">22</span>
-            </div>
-          </section>
-          <section>
-            <BookmarkBorderIcon />
-          </section>
-        </footer>
-      )}
-    </article>
+        <main>
+          {pathname !== "/explore" && <p className="mb-5">{post?.desc}</p>}
+          <img
+            onClick={handleShowModel}
+            className="rounded-md h-[280px] w-full object-cover"
+            src={post?.img}
+            alt=""
+          />
+        </main>
+        {pathname !== "/explore" && (
+          <footer className="flex justify-between mt-5">
+            <section className="flex gap-8">
+              <div>
+                <FavoriteBorderIcon />
+                <span className="ml-2">{post?.likes.length}</span>
+              </div>
+              <div>
+                <CommentIcon />
+                <span className="ml-2">{post?.comments.length}</span>
+              </div>
+            </section>
+            <section>
+              <BookmarkBorderIcon />
+            </section>
+          </footer>
+        )}
+      </article>
+
+      {/*  */}
+
+      <SinglePostModel post={post} setOpen={setOpen} open={open} />
+    </>
   );
 };
 
